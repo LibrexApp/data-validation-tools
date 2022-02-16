9eff4fea016d37569a33a4d80ab5ebcded881c22
Tyler <tyler.roland92@gmail.com>
2022-02-16 17:44:04 -0500

    update unit tests with new validation cases

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,87 +1,151 @@
-import { EDataType } from '../edata-types';
-import { DataValidator } from './../index';
+import { EDataType } from "../edata-types";
+import { DataValidator } from "./../index";
 
 // For testing purposes
 const PersonSchema = [
-    {
-        key: 'name',
-        required: true,
-        type: EDataType.STRING,
-    },
-    {
-        key: 'age',
-        required: true,
-        type: EDataType.NUMBER,
-    },
-    {
-        key: 'male',
-        required: true,
-        type: EDataType.BOOLEAN,
-    },
-    {
-        key: 'middleName',
-        required: false,
-        type: EDataType.STRING,
-    },
-    {
-        key: 'weight',
-        required: false,
-        type: EDataType.NUMBER,
-    },
-    {
-        key: 'alive',
-        required: false,
-        type: EDataType.BOOLEAN,
-    },
+  {
+    key: "name",
+    required: true,
+    type: EDataType.STRING,
+  },
+  {
+    key: "age",
+    required: true,
+    type: EDataType.NUMBER,
+  },
+  {
+    key: "male",
+    required: true,
+    type: EDataType.BOOLEAN,
+  },
+  {
+    key: "middleName",
+    required: false,
+    type: EDataType.STRING,
+  },
+  {
+    key: "weight",
+    required: false,
+    type: EDataType.NUMBER,
+  },
+  {
+    key: "alive",
+    required: false,
+    type: EDataType.BOOLEAN,
+  },
+  {
+    key: "followers",
+    required: false,
+    type: EDataType.NUMBER,
+    minValue: 10,
+  },
+  {
+    key: "parkingTickets",
+    required: false,
+    type: EDataType.NUMBER,
+    maxValue: 2,
+  },
+  {
+    key: "password",
+    required: false,
+    type: EDataType.STRING,
+    minLength: 8,
+  },
+  {
+    key: "zipcode",
+    required: false,
+    type: EDataType.STRING,
+    maxLength: 8,
+  },
 ];
 
-describe('DataValidator', () => {
-    it('Should return "name is required"', () => {
-        const invalidPerson = {
-            age: 21,
-            male: true,
-        };
-        const result = DataValidator(invalidPerson, PersonSchema);
-        expect(result[0]).toEqual('name is required');
-    });
+describe("DataValidator", () => {
+  it('Should return "name is required"', () => {
+    const invalidPerson = {
+      age: 21,
+      male: true,
+    };
+    const result = DataValidator(invalidPerson, PersonSchema);
+    expect(result[0]).toEqual("name is required");
+  });
 
-    it('Should return "name must be a string"', () => {
-        const invalidPerson = {
-            name: 123,
-            age: 21,
-            male: true,
-        };
-        const result = DataValidator(invalidPerson, PersonSchema);
-        expect(result[0]).toEqual('name must be a string');
-    });
-    it('Should return "age must be a number"', () => {
-        const invalidPerson = {
-            name: '123',
-            age: '21',
-            male: true,
-        };
-        const result = DataValidator(invalidPerson, PersonSchema);
-        expect(result[0]).toEqual('age must be a number');
-    });
-    it('Should return "male must be a boolean"', () => {
-        const invalidPerson = {
-            name: '123',
-            age: 21,
-            male: 'yo momma',
-        };
-        const result = DataValidator(invalidPerson, PersonSchema);
-        expect(result[0]).toEqual('male must be a boolean');
-    });
-    it('Should return an errors array of length 6 because 6 invalid options were supplied', () => {
-        const invalidPerson = {
-            name: 123,
-            age: 'yo mom',
-            male: 69,
-            middleName: true,
-            alive: 'hasjdfkhdaskjhlk',
-            weight: 'this is a string!',
-        };
-        const result = DataValidator(invalidPerson, PersonSchema);
-        expect(result !== true ? result.length : []).toEqual(6);
-    });
+  it('Should return "name must be a string"', () => {
+    const invalidPerson = {
+      name: 123,
+      age: 21,
+      male: true,
+    };
+    const result = DataValidator(invalidPerson, PersonSchema);
+    expect(result[0]).toEqual("name must be a string");
+  });
+  it('Should return "age must be a number"', () => {
+    const invalidPerson = {
+      name: "123",
+      age: "21",
+      male: true,
+    };
+    const result = DataValidator(invalidPerson, PersonSchema);
+    expect(result[0]).toEqual("age must be a number");
+  });
+  it('Should return "male must be a boolean"', () => {
+    const invalidPerson = {
+      name: "123",
+      age: 21,
+      male: "yo momma",
+    };
+    const result = DataValidator(invalidPerson, PersonSchema);
+    expect(result[0]).toEqual("male must be a boolean");
+  });
+  it(`Should return "followers must be 10 or more"`, () => {
+    const invalidPerson = {
+      name: "James Bond",
+      age: 30,
+      male: true,
+      followers: 5,
+    };
+    const result = DataValidator(invalidPerson, PersonSchema);
+    expect(result[0]).toEqual("followers must be 10 or more");
+  });
+  it(`Should return "parkingTickets must be 2 or less"`, () => {
+    const invalidPerson = {
+      name: "Donkey Kong",
+      age: 22,
+      male: false,
+      parkingTickets: 5,
+    };
+    const result = DataValidator(invalidPerson, PersonSchema);
+    expect(result[0]).toEqual("parkingTickets must be 2 or less");
+  });
+  it(`Should return "password must be 8 characters or more"`, () => {
+    const invalidPerson = {
+      name: "Hacker Jon",
+      age: 22,
+      male: true,
+      password: "1234",
+    };
+    const result = DataValidator(invalidPerson, PersonSchema);
+    expect(result[0]).toEqual("password must be 8 characters or more");
+  });
+  it(`Should return "zipcode must be 8 characters or less"`, () => {
+    const invalidPerson = {
+      name: "Carmen Sandiego",
+      age: 22,
+      male: false,
+      zipcode: "1A2B3C4D5E",
+    };
+    const result = DataValidator(invalidPerson, PersonSchema);
+    expect(result[0]).toEqual("zipcode must be 8 characters or less");
+  });
+  it("Should return an errors array of length 6 because 6 invalid options were supplied", () => {
+    const invalidPerson = {
+      name: 123,
+      age: "yo mom",
+      male: 69,
+      middleName: true,
+      alive: "hasjdfkhdaskjhlk",
+      weight: "this is a string!",
+    };
+    const result = DataValidator(invalidPerson, PersonSchema);
+    expect(result !== true ? result.length : []).toEqual(6);
+  });
 });
