8b2c3e52ea80f0c549053b0dcb1c127eede48db9
Andrew Pynch <andrewpynchbusiness@gmail.com>
2022-02-16 17:22:07 -0600

    increment package version and add rollup for bundling

--- lib/validators.d.ts
+++ lib/validators.d.ts
@@ -4,3 +4,5 @@ export declare function isInt(x: any, key: any): string | true;
 export declare function isBool(x: any, key: any): string | true;
 export declare function maxLength(x: any, key: any, max: any): string | true;
 export declare function minLength(x: any, key: any, min: any): string | true;
+export declare function maxValue(x: any, key: any, max: any): string | true;
+export declare function minValue(x: any, key: any, min: any): string | true;
