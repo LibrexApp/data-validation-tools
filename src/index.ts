046c370ad98b9ea56c861d74a74e481a0a66feee
Tyler <tyler.roland92@gmail.com>
2022-02-16 17:31:14 -0500

    Added additional validation - min/max value, min/max length

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,66 @@
-import { EDataType } from './edata-types';
-import { ISchemaOption } from './ISchemaOption';
-import { isBool, isInt, isString, notEmpty } from './validators';
+import { EDataType } from "./edata-types";
+import { ISchemaOption } from "./ISchemaOption";
+import {
+  isBool,
+  isInt,
+  isString,
+  notEmpty,
+  minLength,
+  maxLength,
+  minValue,
+  maxValue,
+} from "./validators";
 
 export const DataValidator = (payload: any, schemaOptions: ISchemaOption[]) => {
-    const results = [];
+  const results = [];
 
-    schemaOptions.map((schemaOption: ISchemaOption) => {
-        const key = schemaOption.key;
-        const value = payload[key]; // value from payload
+  schemaOptions.map((schemaOption: ISchemaOption) => {
+    const key = schemaOption.key;
+    const value = payload[key]; // value from payload
+    const hasValue = value !== null && value !== undefined;
 
-        if (schemaOption.required) {
-            results.push(notEmpty(value, key));
-        }
+    if (schemaOption.required) {
+      results.push(notEmpty(value, key));
+    }
 
-        if (value !== null && value !== undefined) {
-            //type checking
-            switch (schemaOption.type) {
-                case EDataType.STRING:
-                    results.push(isString(value, key));
-                    break;
-                case EDataType.NUMBER:
-                    results.push(isInt(value, key));
-                    break;
-                case EDataType.BOOLEAN:
-                    results.push(isBool(value, key));
-                    break;
-                default:
-                    break;
-            }
-        }
-    });
+    if (hasValue) {
+      //type checking
+      switch (schemaOption.type) {
+        case EDataType.STRING:
+          results.push(isString(value, key));
+          break;
+        case EDataType.NUMBER:
+          results.push(isInt(value, key));
+          break;
+        case EDataType.BOOLEAN:
+          results.push(isBool(value, key));
+          break;
+        default:
+          break;
+      }
 
-    const filteredResults = results.filter((e) => e !== true);
-    return filteredResults.length == 0 ? true : filteredResults;
+      //minLength
+      if (schemaOption.hasOwnProperty("minLength")) {
+        results.push(minLength(value, key, schemaOption.minLength));
+      }
+
+      //maxLength
+      if (schemaOption.hasOwnProperty("maxLength")) {
+        results.push(maxLength(value, key, schemaOption.maxLength));
+      }
+
+      //maxValue
+      if (schemaOption.hasOwnProperty("maxValue")) {
+        results.push(maxValue(value, key, schemaOption.maxValue));
+      }
+
+      //minValue
+      if (schemaOption.hasOwnProperty("minValue")) {
+        results.push(minValue(value, key, schemaOption.minValue));
+      }
+    }
+  });
+
+  const filteredResults = results.filter((e) => e !== true);
+  return filteredResults.length == 0 ? true : filteredResults;
 };
